20a1210c8d5c84fbbd58e8bc8da1218c44584650
Akash.T <akashchicku4@gmail.com>
2026-04-16 23:22:48 +0530

    Implemented Dynamic HTML Generation for Checkout Page

--- data/cart.js
+++ data/cart.js
@@ -1,1 +1,18 @@
-export const cart = [];
+export const cart = [
+  {
+    productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+    productQuantity: 2,
+  },
+  {
+    productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
+    productQuantity: 1,
+  },
+   {
+    productId: "83d4ca15-0f35-48f5-b7a3-1ea210004f2e",
+    productQuantity: 2,
+  },
+  {
+    productId: "54e0eccd-8f36-462b-b68a-8182611d9add",
+    productQuantity: 1,
+  },
+];
